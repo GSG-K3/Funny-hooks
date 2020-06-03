c4fba51c94886a90f1f7b21e0a546770ca5a5785
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-06-03 20:22:05 +0200

    change home class to function

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import people from '../../images/laugh.png';
 import './Home.css';
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <div className='logo'>Funny Hooks.</div>
-        <div className='title-img'>
-          <div className='title'>
-            Your daily does of <span>Jokes</span>
-          </div>
-          <img className='home-img' src={people} alt='two people laughing' />
+const Home = () => {
+  return (
+    <div>
+      <div className='logo'>Funny Hooks.</div>
+      <div className='title-img'>
+        <div className='title'>
+          Your daily does of <span>Jokes</span>
         </div>
+        <img className='home-img' src={people} alt='two people laughing' />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default Home;
