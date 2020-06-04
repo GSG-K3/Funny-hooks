ed0b97c29fcfa507c35bd41e8f7ada65305c8994
bayan-404 <bayan.jubah@gmail.com>
2020-06-04 13:32:10 +0300

    add the joke component #12

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import Category from './category'
 import Category from './component/category'
+
 function App() {  
   return (
     <div className="App">
---
+++ src/component/Joke.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Joke =({jokeText}) =>{
+  return (
+  <p>{jokeText}</p>
+  )
+}
+
+export default Joke;
